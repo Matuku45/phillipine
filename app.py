48c07d4b561a53820a47bbfe482174e45186497d
Manuel Mapoulo <150783819+Matuku45@users.noreply.github.com>
2025-08-15 15:57:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"  # Direct text response
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment
+    app.run(debug=True, host='0.0.0.0', port=port)
